ee04eb60195c1c4bbb97b29bf9e39251c851a820
metalsergs <127248964+metalsergs@users.noreply.github.com>
2023-03-08 14:13:34 +0000

    Update aiplusmy.py

--- aiplusmy.py
+++ aiplusmy.py
@@ -82,44 +82,86 @@ def play_game():
             print(f'Player\'s hand: {player_hand}')
             print("Player's score is " + str(calculate_hand_value(player_hand)))
             print(f'Dealer\'s hand: [\'{dealer_hand[0]}\', \'?\']')
-            while True:
-                decision = input('1 - hit, 2 - stand: ')
-                if decision == '1':
-                    player_hand += deal_cards(1)
-                    print(f'Player\'s hand: {player_hand}')
-                    print("Player's score is " + str(calculate_hand_value(player_hand)))
-                    if calculate_hand_value(player_hand) > 21:
-                        print('Player busts! Dealer wins.')
-                        bankroll -= bet
+            if bet <= bankroll / 2:
+                while True:
+                    decision = input('1 - hit, 2 - stand, 3 - double down: ')
+                    if decision == '1':
+                        player_hand += deal_cards(1)
+                        print(f'Player\'s hand: {player_hand}')
+                        print("Player's score is " + str(calculate_hand_value(player_hand)))
+                        if calculate_hand_value(player_hand) > 21:
+                            print('Player busts! Dealer wins.')
+                            bankroll -= bet
+                            break
+                        else:
+                            print(f'Dealer\'s hand: [\'{dealer_hand[0]}\', \'?\']')
+                    elif decision == '2':
+                        print("Player's final score is " + str(calculate_hand_value(player_hand)))
+                        break
+                    elif decision == "3":
+                        player_hand += deal_cards(1)
+                        print("Your final bet is " + str(bet * 2))
+                        print(f'Player\'s hand: {player_hand}')
+                        print("Player's final score is " + str(calculate_hand_value(player_hand)))
+                        if calculate_hand_value(player_hand) > 21:
+                            print('Player busts! Dealer wins.')
+                            bankroll -= bet * 2
                         break
                     else:
-                        print(f'Dealer\'s hand: [\'{dealer_hand[0]}\', \'?\']')
-                elif decision == '2':
-                    print("Player's final score is " + str(calculate_hand_value(player_hand)))
-                    break
-                else:
-                    print("Invalid input, please enter 1 or 2.")
-                    continue
+                        print("Invalid input, please enter 1, 2 or 3.")
+                        continue
+            else:
+                while True:
+                    decision = input('1 - hit, 2 - stand: ')
+                    if decision == '1':
+                        player_hand += deal_cards(1)
+                        print(f'Player\'s hand: {player_hand}')
+                        print("Player's score is " + str(calculate_hand_value(player_hand)))
+                        if calculate_hand_value(player_hand) > 21:
+                            print('Player busts! Dealer wins.')
+                            bankroll -= bet
+                            break
+                        else:
+                            print(f'Dealer\'s hand: [\'{dealer_hand[0]}\', \'?\']')
+                    elif decision == '2':
+                        print("Player's final score is " + str(calculate_hand_value(player_hand)))
+                        break
+                    else:
+                        print("Invalid input, please enter 1 or 2.")
+                        continue
 
             # Dealer's turn
             if not calculate_hand_value(player_hand) > 21:
                 print(f'Dealer\'s hand: {dealer_hand}')
                 print("Dealer's score is " + str(calculate_hand_value(dealer_hand)))
-                while calculate_hand_value(dealer_hand) < 17:
+                while calculate_hand_value(dealer_hand) < 17 and calculate_hand_value(dealer_hand) <= calculate_hand_value(player_hand):
                     dealer_hand += deal_cards(1)
                     print(f'Dealer\'s hand: {dealer_hand}')
                     print("Dealer's score is " + str(calculate_hand_value(dealer_hand)))
-                if calculate_hand_value(dealer_hand) > 21:
-                    print('Dealer busts! Player wins.')
-                    bankroll += bet
-                elif calculate_hand_value(dealer_hand) > calculate_hand_value(player_hand):
-                    print('Dealer wins.')
-                    bankroll -= bet
-                elif calculate_hand_value(dealer_hand) < calculate_hand_value(player_hand):
-                    print('Player wins.')
-                    bankroll += bet
+                if decision == "3":
+                    if calculate_hand_value(dealer_hand) > 21:
+                        print('Dealer busts! Player wins.')
+                        bankroll += bet
+                    elif calculate_hand_value(dealer_hand) > calculate_hand_value(player_hand):
+                        print('Dealer wins.')
+                        bankroll -= bet
+                    elif calculate_hand_value(dealer_hand) < calculate_hand_value(player_hand):
+                        print('Player wins.')
+                        bankroll += bet
+                    else:
+                        print('Push.')
                 else:
-                    print('Push.')
+                    if calculate_hand_value(dealer_hand) > 21:
+                        print('Dealer busts! Player wins.')
+                        bankroll += bet * 2
+                    elif calculate_hand_value(dealer_hand) > calculate_hand_value(player_hand):
+                        print('Dealer wins.')
+                        bankroll -= bet * 2
+                    elif calculate_hand_value(dealer_hand) < calculate_hand_value(player_hand):
+                        print('Player wins.')
+                        bankroll += bet * 2
+                    else:
+                        print('Push.')
             break
 
         # Check if the player wants to play again
